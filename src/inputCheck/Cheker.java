6443f6a44fbd8aa6c0f0e05034c38cb32f8240a0
ezotkina <ezotkina@umiacs.umd.edu>
2016-03-09 23:39:06 -0500

    New command line argument -- -d -- to provide the directory with the data challenge source files (FFIEC.csv etc).
    Signed-off-by: ezotkina <ezotkina@umiacs.umd.edu>

---
+++ src/inputCheck/Cheker.java
@@ -0,0 +1,256 @@
+package inputCheck;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.TreeSet;
+
+
+public class Cheker {
+	private String logDir = "logs//";
+	private Task task;
+	private LogWriter log;
+	private ArrayList<String> TP_IDs;
+	private ArrayList<File> filesToCheck;
+	
+	public Cheker(int TASK, ArrayList<File> files) {
+		this.filesToCheck = files;
+		
+		task = new Task(files.get(0)); // pass the data directory
+		
+		if (TASK == MainRun.TASK1){
+			task.setName1("FFIEC");
+			task.setName2("LEI");
+			task.setIDcolName1("FFIEC_IDRSSD");
+			task.setIDcolName2("LEI_LEI");			
+		}
+		else if (TASK == MainRun.TASK2){
+			task.setName1("FFIEC");
+			task.setName2("SEC");
+			task.setIDcolName1("FFIEC_IDRSSD");
+			task.setIDcolName2("SEC_CIK");
+		}
+		else if (TASK == MainRun.TASK3){			
+			task.setName1("FFIEC");
+			task.setName2("LEI");
+			task.setName3("SEC");
+			task.setIDcolName1("FFIEC_IDRSSD");
+			task.setIDcolName2("LEI_LEI");
+			task.setIDcolName3("SEC_CIK");
+		}
+		else if (TASK == MainRun.TASK4){			
+			task.setName1("LEI");
+			task.setName2("SEC");
+			task.setIDcolName1("LEI_LEI");
+			task.setIDcolName2("SEC_CIK");
+		}
+			
+	}
+	
+	public boolean check(){
+		return this.check(this.filesToCheck);
+	}
+	
+	private boolean check(ArrayList<File> files){
+		
+		boolean errorFound = false;
+		
+		for (File f:files){
+			if (isTPfile(f)){
+				System.out.println("\n***Start checking TP file: "+f);
+				openLog(f);
+				errorFound = checkTP(f);
+				closeLog();
+				System.out.println("End checking TP file: "+f);
+			}
+			
+			try {
+				// waiting is just to have clean console printout. 
+				// Since System.out and System.err are redirected, console printout lines 
+				// can be mixed up.
+				Thread.sleep(50);				
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			
+			if (isTNfile(f)){
+				System.out.println("\n***Start checking TN file: "+f);
+				openLog(f);				
+				errorFound = checkTN(f);
+				closeLog();
+				System.out.println("End checking TN file: "+f);
+			}
+		}
+		
+		return errorFound;
+	}
+	
+	private boolean isTPfile(File f){
+		return f.getName().toUpperCase().contains("_TP");
+	}
+	
+	private boolean isTNfile(File f){
+		return f.getName().toUpperCase().contains("_TN");
+	}
+	
+	private boolean checkTN(File file){
+		TreeSet<String> addSuccess = new TreeSet<String>();
+		boolean errorFound = false;
+		
+		try {
+			BufferedReader br = new BufferedReader(new FileReader(file));
+			
+			String line = br.readLine(); // read first line; it must be header;
+			
+			if (!line.trim().equalsIgnoreCase(task.getIDcolName1())){
+				System.err.println("Warning: possibly no header");
+				errorFound = true;
+			}
+			
+			int lineCount = 2;
+			
+			// read the rest of the file
+			while((line=br.readLine()) != null){
+				line = line.trim();			
+				
+				if (line.isEmpty()){
+					System.err.println("Warning on line "+lineCount+": empty line");
+					errorFound = true;
+				}
+				else if (!addSuccess.add(line)){ // the add() returns false if such item exists
+					System.err.println("Error on line "+lineCount+": duplicate entry -- "+line);
+					errorFound = true;
+				}
+				else if (!this.task.getID_list1().contains(line)){
+					System.err.println("Error on line "+lineCount+": ID not found in "+task.getName1()+" -- "+line);
+					errorFound = true;
+				}
+				else if (TP_IDs != null && TP_IDs.contains(line)){
+					System.err.println("Error on line "+lineCount+": ID is present in both *_TP and *_TN files -- "+line);
+					errorFound = true;
+				}
+				
+				lineCount++;
+			}
+			
+			br.close();
+			
+			if (!errorFound)
+				System.out.println("Success! No errors found.");
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		return errorFound;
+	}
+
+	private boolean checkTP(File file){
+		TreeSet<String> addSuccess = new TreeSet<String>();
+		TP_IDs = new ArrayList<String>();
+		
+		boolean errorFound = false;
+		
+		try {
+			BufferedReader br = new BufferedReader(new FileReader(file));
+			
+			String line = br.readLine(); // read first line; it must be header;
+			String[] tokens = cleanLine(line).split(",");
+			
+			// check header
+			if (!tokens[0].equalsIgnoreCase(task.getIDcolName1()) || !tokens[1].equalsIgnoreCase(task.getIDcolName2())){
+				System.err.println("Warning: no or wrong header");
+				errorFound = false;
+			}
+			
+			int lineCount = 2;
+			
+			// read the rest of the file
+			while((line=br.readLine()) != null){
+				line = cleanLine(line);		
+				tokens = line.split(",");
+				String ID1 = null;
+				String ID2 = null;
+				String ID3 = null;
+				
+				int elements = 0;
+				
+				for (String s:tokens){
+					if (!s.trim().isEmpty())
+						elements++;
+				}
+				
+				if (elements == 2){ 
+					ID1 = tokens[0].trim();
+					ID2 = tokens[1].trim();
+				}
+				else if (elements == 3){ 
+					ID1 = tokens[0].trim();
+					ID2 = tokens[1].trim();
+					ID3 = tokens[2].trim();
+				}
+				
+								
+				if (line.isEmpty()){
+					System.err.println("Warning on line "+lineCount+": empty line");
+					errorFound = true;
+				}
+				else if (elements != task.getElemNum()){
+					System.err.println("Error on line "+lineCount+": wrong number of elements -- "+line);
+					errorFound = true;
+				}
+				else if (!addSuccess.add(line)){ // add returns false if such item exists
+					System.err.println("Error on line "+lineCount+": duplicate entry -- "+line);
+					errorFound = true;
+				}				
+				else{
+					if (!this.task.getID_list1().contains(ID1)){
+						System.err.println("Error on line "+lineCount+": ID not found in "+task.getName1()+" -- "+ID1);
+						errorFound = true;
+					}
+					
+					if (!this.task.getID_list2().contains(ID2)){
+						System.err.println("Error on line "+lineCount+": ID not found in "+task.getName2()+" -- "+ID2);
+						errorFound = true;
+					}
+					
+					if (this.task.getID_list3() != null && !this.task.getID_list3().contains(ID3)){
+						System.err.println("Error on line "+lineCount+": ID not found in "+task.getName3()+" -- "+ID3);
+						errorFound = true;
+					}
+				}
+				
+				TP_IDs.add(ID1);
+				
+				lineCount++;
+			}
+			
+			br.close();
+			if (!errorFound)
+				System.out.println("Success! No errors found.");
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		return errorFound;
+	}
+	
+	// remove whitespaces 
+	private String cleanLine(String line){
+		return line.trim().replaceAll("\\s+", "");
+	}
+	
+	private void openLog(File file) {				
+		log = new LogWriter(logDir+file+".log");		
+	}
+	
+	private void closeLog() {				
+		log.close();		
+	}
+
+}
---
+++ src/inputCheck/MainRun.java
@@ -0,0 +1,213 @@
+package inputCheck;
+import java.io.File;
+import java.util.ArrayList;
+
+
+public class MainRun {
+	private final String t1 = "-t1";
+	private final String t2 = "-t2";
+	private final String t3 = "-t3";
+	private final String t4 = "-t4";
+	
+	public static final int TASK1 = 1;
+	public static final int TASK2 = 2;
+	public static final int TASK3 = 3;
+	public static final int TASK4 = 4;
+		
+	private int TASK = 0;
+	
+	private String dataDir;
+	
+	public static void main(String[] args) {
+				
+		MainRun mr = new MainRun();
+				
+		mr.dataDir = null;
+		
+		boolean errorFound = mr.processArgs(args);			
+			
+		if (errorFound)
+			System.exit(1); // exit with error
+		
+		Cheker ch = new Cheker(mr.TASK, mr.getArgFiles(args));
+		
+		errorFound = ch.check();
+				
+		System.exit(errorFound ? 1 : 0); // exit with error if errors found
+	}
+	
+	private boolean processArgs(String[] argsStr){
+		ArrayList<String> args = new ArrayList<String>();
+		
+		for (String s:argsStr)
+			args.add(s);
+		
+		if (args.contains(t1))
+			TASK = TASK1;
+		else if (args.contains(t2))
+			TASK = TASK2;
+		else if (args.contains(t3))
+			TASK = TASK3;
+		else if (args.contains(t4))
+			TASK = TASK4;
+		
+		return checkArgs(args);
+		
+	}
+
+	private boolean checkArgs(ArrayList<String> args){
+		boolean errorFound = false;
+		
+		if (args.size() == 0){
+			System.err.println("No arguments.");
+			errorFound = true;
+		}
+		else if (!isDataDirValid(args))
+			errorFound = true;
+		else if (TASK == 0){
+			System.err.println("No task argument (-t1|t2|t3|t4).");
+			errorFound = true;
+		}
+		else if (args.size() > 5 || args.size() < 2){
+			System.err.println("Wrong number of arguments.");
+			errorFound = true;
+		}		
+		else if (args.size() <= 3){
+			ArrayList<String> files = new ArrayList<String>(2);
+			
+			for (String arg:args){
+				if (arg.startsWith("-t"))
+					continue;
+				
+				File f = new File(arg);
+				if (!f.exists()){
+					System.err.println("File doesn't not exist: "+f);
+					errorFound = true;
+				}
+				
+				files.add(f.getName());				
+			}
+			
+			boolean correctVersion = checkFileVersion(files);
+			boolean correctFileName = checkFileName(files);
+			boolean correctOrgID = checkOrgID(files);
+			
+			if (!correctFileName){
+				System.err.println("Filename(s) don't satisfy naming rule: "+files);
+				errorFound = true;
+			}
+			
+			if (!correctVersion){
+				System.err.println("*_TP and *_TN are of different version: "+files);
+				errorFound = true;
+			}
+			
+			if (!correctOrgID){
+				System.err.println("*_TP and *_TN have different ORG ID: "+files);
+				errorFound = true;
+			}
+			
+		}
+		
+		if (errorFound)
+			printUsage();
+			
+		
+		
+		return errorFound;
+	}
+	private boolean isDataDirValid(ArrayList<String> args){
+		for (int i=0; i<args.size(); i++){
+			if (args.get(i).contains("-d")){
+				this.dataDir = args.get(i+1);
+				break;
+			}
+		}
+		
+		if (this.dataDir != null){
+			File f = new File(this.dataDir);
+			boolean exists = f.exists() && f.isDirectory();
+			if (!exists)
+				System.err.println("Data directory doesn't exists:\n"+this.dataDir);
+			
+			return exists;
+		}
+		else
+			return true;			
+	}
+	
+	private boolean checkFileName(ArrayList<String> files){
+		String check = "";
+		if (TASK == TASK1)
+			check = "_FFIEC_LEI_";
+		else if (TASK == TASK2)
+			check = "_FFIEC_SEC_";
+		else if (TASK == TASK3)
+			check = "_FFIEC_LEI_SEC_";
+		else if (TASK == TASK4)
+			check = "_LEI_SEC_";
+		
+		
+		for (String fname:files){
+			if (!fname.toUpperCase().contains(check))
+				return false;
+		}
+		
+		return true;
+		
+	}
+	
+	private boolean checkFileVersion(ArrayList<String> files){
+		if (files.size() == 1)
+			return true;
+		
+		String name1 = files.get(0);
+		String name2 = files.get(1);
+		
+		int index1 = name1.lastIndexOf("_")+1;
+		int index2 = name1.lastIndexOf(".");
+		
+		String version1 = name1.substring(index1, index2).trim();
+		
+		index1 = name2.lastIndexOf("_")+1;
+		index2 = name2.lastIndexOf(".");
+		
+		String version2 = name2.substring(index1, index2).trim();
+				
+		return version1.equalsIgnoreCase(version2);
+	}
+	
+	private boolean checkOrgID(ArrayList<String> files){
+		if (files.size() == 1)
+			return true;
+		
+		String name1 = files.get(0).split("_", 2)[0];
+		String name2 = files.get(1).split("_", 2)[0];
+						
+		return name1.equalsIgnoreCase(name2);
+	}
+	
+	private void printUsage(){
+		System.out.println("Usage:\n" +
+				"java -jar Checker.jar [-d <dir>] -t1|t2|t3|t4 <ORG_*_TP_X.csv> [ORG_*_TN_X.scv]\n\n" +
+				"Task 1: -d <dir> -t1 <ORG_FFIEC_LEI_TP_X.csv> [ORG_FFIEC_LEI_TN_X.csv]\n" + 
+				"Task 2: -d <dir> -t2 <ORG_FFIEC_SEC_TP_X.csv> [ORG_FFIEC_SEC_TN_X.csv]\n" +
+				"Task 3: -d <dir> -t3 <ORG_FFIEC_LEI_SEC_TP_X.csv> [ORG_FFIEC_LEI_SEC_TN_X.csv]\n" +
+				"Task 4: -d <dir> -t4 <ORG_LEI_SEC_TP_X.csv> [ORG_LEI_SEC_TN_X.csv]\n" +
+				"-d <dir>: the directory where FFIEC.csv, LEI.csv, and SEC.csv are located.\n");
+				
+	}
+	
+	private ArrayList<File> getArgFiles(String[] args){
+		ArrayList<File> files = new ArrayList<File>(2);
+		
+		for (int i=0; i<args.length; i++){
+			if (args[i].contains("-d"))
+				files.add(0, new File(args[i+1])); // first file in the list is the data directory			
+			else if (args[i].contains("-t"))
+				files.add(new File(args[i+1]));			
+		}
+		
+		return files;
+	}
+}
